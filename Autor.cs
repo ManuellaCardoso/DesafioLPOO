a683fa746024dd7db1fdaf79cb27012eb4b123d2
Manuella S. Cardoso <163538424+ManuellaCardoso@users.noreply.github.com>
2024-06-19 08:21:48 -0300

    first commit

---
+++ Autor.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Autor {
+    public string Nome;
+    public string Nacionalidade;
+
+    //Construtor 
+    public Autor (string nome, string nacionalidade) {
+        Nome = nome;
+        Nacionalidade = nacionalidade;
+    }
+
+    public void MostrarInfo(){
+        Console.WriteLine($"Nome: {Nome}, Nacionalidade: {Nacionalidade}");
+    }
+}
